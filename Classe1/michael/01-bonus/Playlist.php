1db59f6021723bec4052135f32d3325870fca09b
WebDevCF2m <gitweb@cf2m.be>
2026-09-04 12:08:08 +0200

    Classe1/michael/01-bonus

--- Classe1/michael/01-bonus/Playlist.php
+++ Classe1/michael/01-bonus/Playlist.php
@@ -1,5 +1,26 @@
 <?php
 
 class Playlist{
-    
+    // Propriétés
+    private array $chansons = []; // conteneur de chanson
+    private int $dureeTotal = 0; // conteneur de durée totale
+
+    // Constantes
+
+    // Méthodes
+
+    // ajouter les chansons
+    public function ajouter(Chanson $item):void
+    {
+        $this->chansons[] = $item;
+    }
+    // afficher les chansons
+    public function afficher():string
+    {
+        $content = "";
+        foreach($this->chansons as $chanson){
+            
+        }
+        return "";
+    }
 }
