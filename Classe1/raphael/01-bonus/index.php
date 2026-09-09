b7ae5d06354f8c3864e0d673edf8532699656267
raphaeldebot <debot.raphael@gmail.com>
2026-09-09 10:16:19 +0200

    ex 1 bonus

--- Classe1/raphael/01-bonus/index.php
+++ Classe1/raphael/01-bonus/index.php
@@ -11,4 +11,5 @@ $playlist->ajouter(new Chanson('Get Lucky', 'Daft Punk', 248));
 $playlist->ajouter(new Chanson('Redbone', 'Childish Gambino', 327));
 
 $playlist->afficher();
+echo "<hr>{$playlist->dureeTotale()}<hr>";
 echo 'Durée totale : ' . $playlist->formaterDuree($playlist->dureeTotale()) . PHP_EOL;
